63514dcaacd8dc6a0b9fafb578346de94008e0d5
prottay <prottay.saha46@gmail.com>
2025-11-24 00:56:36 +0600

    search funtionality added in pokemmon fetch project

--- PokemonCards/src/components/pokemons/Pokemons.jsx
+++ PokemonCards/src/components/pokemons/Pokemons.jsx
@@ -6,7 +6,8 @@ const limit = 20;
 const Pokemons = () => {
   const [pokemons, setPokemons] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
+  const [error, setError] = useState(null);
+  const [search, setSearch] = useState("");
 
   const API = `https://pokeapi.co/api/v2/pokemon?limit=${limit}`;
 
@@ -35,6 +36,10 @@ const Pokemons = () => {
     fetchPokemonData();
   }, []);
 
+  const searchData = pokemons.filter((currPokemon) =>
+    currPokemon.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   if (loading) {
     return (
       <div>
@@ -57,9 +62,17 @@ const Pokemons = () => {
         <header>
           <h1>Let's Catch Pokemon</h1>
         </header>
+        <div className="pokemon-search">
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
         <div>
           <ul className="cards">
-            {pokemons.map((currPokemon) => {
+            {/* {pokemons.map((currPokemon) => { */}
+            {searchData.map((currPokemon) => {
               return (
                 <PokemonCard key={currPokemon.id} pokemonData={currPokemon} />
               );
