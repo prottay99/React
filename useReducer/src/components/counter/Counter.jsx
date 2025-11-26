6909567740b1ac2ce1df189c1b70bd17f7260c4c
prottay <prottay.saha46@gmail.com>
2025-11-26 16:58:57 +0600

    some update in useReducer practice file

--- useReducer/src/components/counter/Counter.jsx
+++ useReducer/src/components/counter/Counter.jsx
@@ -1,21 +1,43 @@
 import React, { useReducer } from "react";
 
+const initialState = {
+  count: 0,
+  inc: 2,
+  dec: 2,
+};
+
 const Counter = () => {
-  const reducer = (count, action) => {
-    if (action.type === "INCREMENT") {
-      return count + 1;
-    }
-    if (action.type === "DECREMENT") {
-      return count - 1;
+  const reducer = (state, action) => {
+    // if (action.type === "INCREMENT") {
+    //   return count + 1;
+    // }
+    // if (action.type === "DECREMENT") {
+    //   return count - 1;
+    // }
+    // if (action.type === "RESET") {
+    //   return (count = 0);
+    // }
+
+    switch (action.type) {
+      case "INCREMENT":
+        return { ...state, count: state.count + 1 };
+      case "DECREMENT":
+        return { ...state, count: state.count - 1 };
+      case "RESET":
+        return { ...state, count: 0 };
+      default:
+        return state;
     }
   };
-  // const [count, setCount] = useState(0);
-  const [count, dispatch] = useReducer(reducer, 0);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div>
-      <h2>{count}</h2>
+      <h2>{state.count}</h2>
       <button onClick={() => dispatch({ type: "INCREMENT" })}>Increment</button>
       <button onClick={() => dispatch({ type: "DECREMENT" })}>Decrement</button>
+      <button onClick={() => dispatch({ type: "RESET" })}>Reset</button>
     </div>
   );
 };
